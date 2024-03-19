7cd6cef89ed799e37951fb4c5fcc9a529ddd45de
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2024-03-20 04:05:35 +0600

    initial change

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Numan Zaman Dipu",
+  title: "Alpha Microsoft Learn Student Ambassador | Apprentice Mechanical Engineer",
+  email: "numanzamandipuu@gmail.com",
+  gitHub: "numanzamandipuu",
+  instagram: "numanzamandipuu",
+  linkedIn: "numanzamandipuu",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "numanzamandipuu",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
